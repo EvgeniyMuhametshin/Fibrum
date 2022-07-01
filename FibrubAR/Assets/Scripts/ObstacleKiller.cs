bd3fb381aa2c67758e728dc9cf0f3a5e436ef786
Evgeniy Muhamethin <John090918@gmail.com>
2022-07-02 01:35:57 +0300

    Created Generated Obstacle

--- FibrubAR/Assets/Scripts/ObstacleKiller.cs
+++ FibrubAR/Assets/Scripts/ObstacleKiller.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class ObstacleKiller : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
+    [SerializeField] private Transform _player;
+    [SerializeField] private ObstacleSpawner _spawner;
+    [SerializeField] private float _killDistanceZ;
 
     // Update is called once per frame
     void Update()
     {
+        List<Transform> obstacle = _spawner.spawnedObstacles;
 
+        for (int i = 0; i < obstacle.Count; i++)
+        {
+            if (_player.position.z > obstacle[i].position.z + _killDistanceZ)
+            {
+                Destroy(obstacle[i].gameObject);
+            }
+        }
     }
 }
---
+++ FibrubAR/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+    [SerializeField] private Transform[] _obstacle;
+    [SerializeField] private float _spawnStep;
+    [SerializeField] private float _spawnDistance;
+
+    [SerializeField] private Vector2 _segmentWidth;
+
+    private Transform _myTrans;
+    private Vector3 _lastPos;
+
+    private List<Transform> _spavnerObstacles = new List<Transform>();
+    public List<Transform> spawnedObstacles
+    {
+        get { _spavnerObstacles.RemoveAll(TransformIsNull); return _spavnerObstacles; }
+    }
+
+    bool TransformIsNull(Transform a)
+    {
+        return a == null;
+    }
+
+    // Use this for initialization
+    void Start () {
+        _myTrans = transform;
+        _lastPos = _myTrans.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_myTrans.position.z > _lastPos.z + _spawnStep)
+        {
+            _lastPos.z += _spawnStep;
+
+            Transform newObstacle = _obstacle[Random.Range(0, _obstacle.Length)];
+
+            _spavnerObstacles.Add(Instantiate(newObstacle, new Vector3
+                (Random.Range(_segmentWidth.x, _segmentWidth.y),
+                0, _lastPos.z + _spawnDistance), Quaternion.identity));
+        }
+	}
+}
