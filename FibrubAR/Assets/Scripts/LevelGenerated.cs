85cdff16be84e453d17a7a2e466fce5d3a9edc0b
Evgeniy Muhamethin <John090918@gmail.com>
2022-07-01 08:49:08 +0300

    Generated Level

--- FibrubAR/Assets/Scripts/LevelGenerated.cs
+++ FibrubAR/Assets/Scripts/LevelGenerated.cs
@@ -1,18 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelGenerated : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
+    [SerializeField] private List<Transform> _segments;
+    [SerializeField] private float _minDistans;
+    [SerializeField] private Transform _player;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        Transform lastObject = _segments[_segments.Count - 1];//Обращаемся к последнему элементу в списке
 
+        float dis = Vector3.Distance(lastObject.position, _player.position);
+
+        if (dis < _minDistans)
+        {
+            Transform firstObject = _segments[0];
+            firstObject.position = lastObject.position;
+
+            Vector3 offset = lastObject.GetComponent<Collider>().bounds.extents + firstObject.GetComponent<Collider>().bounds.extents;
+            firstObject.position += Vector3.forward * offset.z;
+
+            _segments.Remove(firstObject);
+            _segments.Add(firstObject);
+        }
     }
 }
